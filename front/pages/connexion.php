b8eb6a294552a333955a07b9e5b7450e264f3995
anne-amelie <anne-amelie.nassiet-combe@isen.yncrea.fr>
2024-12-03 11:17:07 +0100

    sqfqsf

--- front/pages/connexion.php
+++ front/pages/connexion.php
@@ -7,6 +7,7 @@ session_start();
     <head>
         <meta charset="UTF-8"> <!--caractères spéciaux bien affichés-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--adapter tel et tab-->
+        <link rel="icon" type="image/png" href="../../assets/logos/otter.png">
         <title>Login page</title>
         <link rel="stylesheet" href="../style/style1.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
